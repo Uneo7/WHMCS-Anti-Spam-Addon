568ca006640b60c27fdfc687c2665db2362f21ef
Baptiste <Uneo7@users.noreply.github.com>
2019-05-16 17:22:43 +0200

    Update hooks.php
    Fixing upload fail

--- hooks.php
+++ hooks.php
@@ -13,7 +13,23 @@ add_hook('ClientDetailsValidation', 1, function($vars) {
 
 	$code = $res->getStatusCode();
 	if ($code === 200) {
-		$email = json_decode($res->getBody())->disposable ?? null;
+		$email = json_decode($res->getBody());
+		if (isset($email->disposable)) $email = $email->disposable;
+
 		if ($email) return $_ADDONLANG['disposable'];
 	}
+});
+
+add_hook('ClientAreaPage', 1, function($vars) {
+
+	if (!$vars['client']) return;
+
+	if ($vars['client']->emailVerified) return;
+
+	if (isset($_GET['m']) && $_GET['m'] === 'anti_spam') return;
+
+	if (isset($vars['verificationId'])) return;
+
+	header('Location: index.php?m=anti_spam');
+	exit();
 });
